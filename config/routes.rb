8de64b2eaaa765922f354ed9a377ac9792836801
RonaldoMoraes <ronaldo.moraes1990@gmail.com>
2014-11-11 19:15:17 -0200

    adding links to patrimonies and responsables on home#index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :responsibles
 
   resources :patrimonies
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
