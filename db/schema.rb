333d505c31a79c4fe7b98bb6bfd827ff2f075345
pedro <phplucas@gmail.com>
2014-12-04 03:39:59 -0200

    Layout da aplicação melhorada. Logo da Unifei adicionado. e melhor estruturação das tabelas e botões de patrimônios, responsáveis, salas e usuarios.

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111232543) do
+ActiveRecord::Schema.define(version: 20141117213924) do
 
   create_table "patrimonies", force: true do |t|
     t.string   "name"
+    t.string   "identifier"
     t.string   "status"
     t.text     "description"
+    t.integer  "responsible_id"
+    t.integer  "room_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "responsibles", force: true do |t|
+    t.string   "identifier"
     t.string   "name"
     t.string   "email"
+    t.date     "birth"
+    t.string   "address"
+    t.string   "tel"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "rooms", force: true do |t|
-    t.string   "local"
+    t.string   "identifier"
+    t.string   "locale"
+    t.integer  "capacity"
+    t.boolean  "lab"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
